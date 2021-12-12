ec47fc9b43cfba16bdb9fd8ed27ec0404938a027
songw <songw1@chinatelecom.cn>
2021-12-12 22:38:09 +0800

    week2作业

---
+++ src/week2/work/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	sqlCon, err := initSql()
+	defer sqlCon.Close()
+	if err != nil {
+		fmt.Println("initSql error:", err)
+		fmt.Println("%+v\", err, \"\\n")
+	}
+
+	err = queryAc(sqlCon, "select * from account where id=1")
+	if err != nil {
+		fmt.Println("query sql error :", err)
+		fmt.Printf("%+v", err, "\n")
+	}
+}
